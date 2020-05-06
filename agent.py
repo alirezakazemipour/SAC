Resolve action scaling mistake

agent.py
@@ -109,7 +109,7 @@ class SAC:
         states = np.expand_dims(states, axis=0)
         states = from_numpy(states).float().to(self.device)
         action, _ = self.policy_network.sample_or_likelihood(states)
-        return action.detach().cpu().numpy()[0] * self.action_bounds[1]
+        return action.detach().cpu().numpy()[0]  # * self.action_bounds[1]
 
     @staticmethod
     def soft_update_target_network(local_network, target_network, tau=0.005):

main.py
@@ -7,7 +7,7 @@ from torch.utils.tensorboard import SummaryWriter
 from play import Play
 
 # ENV_NAME = "Pendulum-v0"
-ENV_NAME = "Ant-v2"
+ENV_NAME = "Humanoid-v2"
 test_env = gym.make(ENV_NAME)
 
 n_states = test_env.observation_space.shape[0]
@@ -71,24 +71,24 @@ if __name__ == "__main__":
                 action_bounds=action_bounds,
                 reward_scale=reward_scale)
 
-    # for episode in range(1, MAX_EPISODES + 1):
-    #     state = env.reset()
-    #     episode_reward = 0
-    #     done = 0
-    #
-    #     start_time = time.time()
-    #     for _ in range(env._max_episode_steps):
-    #         action = agent.choose_action(state)
-    #         next_state, reward, done, _ = env.step(action)
-    #         agent.store(state, reward, done, action, next_state)
-    #         value_loss, q_loss, policy_loss = agent.train()
-    #         if episode % 250 == 0:
-    #             agent.save_weights()
-    #         if done:
-    #             break
-    #         episode_reward += reward
-    #         state = next_state
-    #     log(episode, start_time, episode_reward, value_loss, q_loss, policy_loss, len(agent.memory))
+    for episode in range(1, MAX_EPISODES + 1):
+        state = env.reset()
+        episode_reward = 0
+        done = 0
+
+        start_time = time.time()
+        for _ in range(env._max_episode_steps):
+            action = agent.choose_action(state)
+            next_state, reward, done, _ = env.step(action)
+            agent.store(state, reward, done, action, next_state)
+            value_loss, q_loss, policy_loss = agent.train()
+            if episode % 250 == 0:
+                agent.save_weights()
+            if done:
+                break
+            episode_reward += reward
+            state = next_state
+        log(episode, start_time, episode_reward, value_loss, q_loss, policy_loss, len(agent.memory))
 
     player = Play(env, agent)
     player.evaluate()