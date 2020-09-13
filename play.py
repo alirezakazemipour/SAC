c66c5d2b0f62c2b77e7b23fb840709d3aac1b216
alirezakazemipour <kazemipour.alireza@gmail.com>
2020-09-13 20:49:36 +0430

    Record frames. *_*

--- play.py
+++ play.py
@@ -4,9 +4,8 @@ import gym
 import time
 from mujoco_py.generated import const
 from mujoco_py import GlfwContext
-GlfwContext(offscreen=True)
-# from pylab import *
 import cv2
+GlfwContext(offscreen=True)
 
 
 class Play:
@@ -17,6 +16,9 @@ class Play:
         self.agent.load_weights()
         self.agent.set_to_eval_mode()
         self.device = device("cuda" if torch.cuda.is_available() else "cpu")
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.VideoWriter = cv2.VideoWriter("Humanoid" + ".avi", self.fourcc, 50.0,
+                                           (500, 500))
 
     def evaluate(self):
 
@@ -38,7 +40,10 @@ class Play:
                 # time.sleep(0.03)
                 I = self.env.render(mode='rgb_array')
                 I = cv2.cvtColor(I, cv2.COLOR_RGB2BGR)
+                self.VideoWriter.write(I)
                 cv2.imshow("env", I)
-                # pause(1 / 120)
                 cv2.waitKey(10)
             print(f"episode reward:{episode_reward:3.3f}")
+        self.env.close()
+        self.VideoWriter.release()
+        cv2.destroyAllWindows()
